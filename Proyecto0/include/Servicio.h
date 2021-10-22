74409c2975e5f564a531a621bbe367b11566f0df
Javier Helo <javierhelo@gmail.com>
2021-10-22 00:37:30 -0600

    Ron con cocaaa
    Juleisy se fue a hablar por telefono jajajajajja

--- Proyecto0/include/Servicio.h
+++ Proyecto0/include/Servicio.h
@@ -7,41 +7,45 @@
 #include <stdexcept>
 
 using std::runtime_error;
+using std::cout;
+using std::endl;
 
 
 class Servicio{
 public:
     string descripcion;
     string codigo;
-    ArrayList<Servicio> *listaServicios;
+//    List<Servicio> *listaServicios = new ArrayList<Servicio>();
 
     Servicio(){
-        listaServicios = new ArrayList<Servicio>();
     }
 
     Servicio(string descripcion, string codigo){
         this->descripcion = descripcion;
         this->codigo = codigo;
-        listaServicios = new ArrayList<Servicio>();
     }
     ~Servicio(){
-        delete listaServicios;
+//        delete listaServicios;
+    }
+    void printServicio(){
+        cout << descripcion << " - " << codigo << endl;
     }
 
-    void agregarServicio(string descripcion, string codigo){
-        Servicio nuevo(descripcion, codigo);
-        listaServicios->append(nuevo);
-    }
+//    void agregarServicio(string descripcion, string codigo){
+//        Servicio nuevo(descripcion, codigo);
+//        listaServicios->append(nuevo);
+//    }
+//
+//    void eliminarServicio(int pos){
+//        if (pos < 0 || pos > listaServicios->getSize()){
+//            throw runtime_error("Seleccione una posicion valida en la lista de servicios.");
+//        }
+//        listaServicios->goToPos(pos);
+//        listaServicios->remove();
+//    }
 
-    void eliminarServicio(int pos){
-        if (pos < 0 || pos > listaServicios->getSize()){
-            throw runtime_error("Seleccione una posicion valida en la lista de servicios.");
-        }
-        listaServicios->goToPos(pos);
-        listaServicios->remove();
-    }
-
-    void reordenar(int posActual, int posDestino){
+    //Función para reordenar servicios
+    void reordenar(int posActual, int posDestino, List<Servicio*> *listaServicios){
         if (posActual < 0 || posActual > listaServicios->getSize()){
             throw runtime_error("Seleccione una posicion valida en la lista de servicios.");
         }
@@ -49,23 +53,25 @@ public:
             throw runtime_error("Seleccione una posicion valida en la lista de servicios.");
         }
         listaServicios->goToPos(posActual);
-        Servicio eliminado = listaServicios->remove();
+        Servicio *eliminado = listaServicios->remove();
+
         listaServicios->goToPos(posDestino);
         listaServicios->insert(eliminado);
     }
-
-    void printS(){
-        cout << "Servicios disponibles: " << endl;
-        for (int i = 0; i < listaServicios->getSize(); i++){
-            listaServicios->goToPos(i);
-            Servicio actual = listaServicios->getElement();
-            cout << i << " - " << actual.descripcion << endl;
-        }
-    }
+//
+//    void printS(){
+//        cout << "Servicios disponibles: " << endl;
+//        for (int i = 0; i < listaServicios->getSize(); i++){
+//            listaServicios->goToPos(i);
+//            Servicio actual = listaServicios->getElement();
+//            cout << i << " - " << actual.descripcion << endl;
+//        }
+//    }
 
     void operator=(const Servicio& other){
         this->descripcion = other.descripcion;
         this->codigo = other.codigo;
+//        this->listaServicios = other.listaServicios;
     }
     bool operator==(const Servicio& other){
         return this->codigo == other.codigo && this->descripcion == other.descripcion;
--- Proyecto0/main.cpp
+++ Proyecto0/main.cpp
@@ -6,14 +6,37 @@
 #include "Tiquete.h"
 
 using namespace std;
+//Función que imprime los tipos de ventanillas de una lista
+void imprimirTotalVentanillas(List<TipoVentanilla*> *totalVentanillas){
+    int i = 0;
+    for(totalVentanillas->goToStart(); !totalVentanillas->atEnd(); totalVentanillas->next()){
+            cout << i << "- ";
+            TipoVentanilla *actual = totalVentanillas->getElement();
+            actual->printVentanilla();
+            cout << endl;
+            i++;
+        }
+}
+//Función que imprime los tipos de servicios de una lista con estos
+void imprimirTotalServicios(List<Servicio*> *totalServicios){
+    int i = 0;
+    for(totalServicios->goToStart(); !totalServicios->atEnd(); totalServicios->next()){
+            cout << i << "- ";
+            Servicio *actual = totalServicios->getElement();
+            actual->printServicio();
+            i++;
+        }
+}
+
 
 int main(){
     setlocale(LC_ALL, "");
 
-    //lista para llevar control de todos los tipos de ventanillas
-    ArrayList<TipoVentanilla> *totalVentanillas = new ArrayList<TipoVentanilla>();
+    //listas para llevar control de todos los Tipos de Ventanillas y Servicios
+    List<TipoVentanilla*> *totalVentanillas = new LinkedList<TipoVentanilla*>();
+    List<Servicio*> *totalServicios = new LinkedList<Servicio*>();
 
-    //Creando tipos de ventanillas arbitrarias
+    //Creando tipos de ventanillas arbitrarias y agregandolas a la lista de totalVentanillas
     string descripcion1 = "Cajas";
     string codigo1 = "C";
     int cantidad1 = 4;
@@ -26,36 +49,47 @@ int main(){
     string codigo3 = "I";
     int cantidad3 = 3;
 
-    TipoVentanilla cajas(descripcion1, codigo1, cantidad1);
-    TipoVentanilla servicioCliente(descripcion2, codigo2, cantidad2);
-    TipoVentanilla informacion(descripcion3, codigo3, cantidad3);
+    //Crearlas
+    TipoVentanilla *caja = new TipoVentanilla(descripcion1, codigo1, cantidad1);
+    TipoVentanilla *servicioCliente = new TipoVentanilla(descripcion2, codigo2, cantidad2);
+    TipoVentanilla *informacion = new TipoVentanilla(descripcion3, codigo3, cantidad3);
 
-    totalVentanillas->append(cajas);
+    //Agregarlas
+    totalVentanillas->append(caja);
     totalVentanillas->append(servicioCliente);
     totalVentanillas->append(informacion);
 
-    cout << "Probando 1..." << endl;
 
-    //Agregando serivicios disponibles
-    Servicio *serviciosDisponibles = new Servicio("", "");
 
-    cout << "2..." << endl;
+    //Agregando servicios disponibles
+    string ddeposito = "deposito";
+    string cdeposito = "C";
 
-    serviciosDisponibles->agregarServicio("Depósito", "C");
-    serviciosDisponibles->agregarServicio("Retiro", "C");
-    serviciosDisponibles->agregarServicio("Solicitar tarjeta", "S");
-    serviciosDisponibles->agregarServicio("Pagar recibo", "C");
-    serviciosDisponibles->agregarServicio("Estado de cuenta", "I");
-    serviciosDisponibles->agregarServicio("Pagar marchamo", "C");
-    serviciosDisponibles->agregarServicio("Cancelar tarjeta", "S");
-    serviciosDisponibles->agregarServicio("Cambio de colones a dólares", "C");
-    serviciosDisponibles->agregarServicio("Cambio de dólares a colones", "C");
-    serviciosDisponibles->agregarServicio("Retirar marchamo", "I");
+    Servicio *deposito = new Servicio(ddeposito, cdeposito);
+    Servicio *retiro = new Servicio("Retiro", "C");
+    Servicio *cambio = new Servicio("Cambio de dólares a colones", "C");
+    Servicio *cambio2 = new Servicio("Cambio de colones a dólares", "C");
+    Servicio *tarjeta = new Servicio("Solicitar tarjeta", "S");
+    Servicio *cancelartarjeta = new Servicio("Cancelar tarjeta", "S");
+    Servicio *cuenta = new Servicio("Estado de cuenta", "I");
+    Servicio *marchamo = new Servicio("Retirar marchamo", "I");
 
-    cout << "3..." << endl;
 
-    serviciosDisponibles->printS();
+    totalServicios->append(deposito);
+    totalServicios->append(retiro);
+    totalServicios->append(cambio);
+    totalServicios->append(cambio2);
+    totalServicios->append(tarjeta);
+    totalServicios->append(cancelartarjeta);
+    totalServicios->append(cuenta);
+    totalServicios->append(marchamo);
 
+    //MENU PRINCIPAL
+
+
+    bool seguirDentro = true;
+    while (seguirDentro){
+    bool regresar = false;     //Variable para poder utilizar la opción de regresar al menú principal
     cout << "Bienvenido al sistema de administración de colas: "<< endl;
     cout << "1. Ver estado de las colas" << endl;
     cout << "2. Solicitar tiquete" << endl;
@@ -64,25 +98,48 @@ int main(){
     cout << "5. Estadísticas del sistema" << endl;
     cout << "6. Salir" << endl;
     cout << "Escriba el número del proceso que desea realizar: " ;
-    int n;
+    int n;      //Chequear que sea un entero (PROOOFEEEEEEE ) "meter un string"
     cin >> n;
+
+    while (n < 1 || n > 6){
+        cout << "El número ingresado no está en las opciones, elija de nuevo que gusta hacer: ";
+        cin >> n;
+    }
     switch(n){
-    case 1:
-        cajas.printVentanilla();
-        servicioCliente.printVentanilla();
-        informacion.printVentanilla();
-
-
+    case 1:         // Mostrar tipos de ventanillas
+        imprimirTotalVentanillas(totalVentanillas);
         break;
 
-    case 2:
+    case 2:         //Solicitar Tiquete
         cout << "a. Seleccionar servicio" << endl;
         cout << "b. Cliente preferencial" << endl;
         cout << "c. Regresar" << endl;
         cout << "Escoja una opción: ";
         char seleccion;
         cin >> seleccion;
+        while (seleccion != 'a' && seleccion != 'b' && seleccion != 'c'){
+            cout << "Escoja una opción válida (a, b, c): ";
+            cin >> seleccion;
+        }
+        switch(seleccion){
+        case 'a':               //El usuario elige un servicio y el sistema lo pone en la cola correspondiente
+            imprimirTotalServicios(totalServicios);
+
+            break;
+        case 'b':               //Se coloca como cliente preferencial para el servicio que seleccione
+        {                       //Se rodea con un bloque de corchetes para poder declarar una variable nueva en el case
+            imprimirTotalServicios(totalServicios);
+            Tiquete *nuevo = new Tiquete();
+
+            break;
+        }
+
+        case 'c':               //Regresa al menú principal
+            regresar = true;
+            break;
+        }
         break;
+
     case 3:
         //Solicitar tipo de ventanilla y numero para buscar en las colas
         //el siguiente tiquete a atender, lo borra y lo atiende actualmente.
@@ -92,6 +149,10 @@ int main(){
         cout << "b. Definir servicios disponibles" << endl;
         cout << "Escoja una opción: ";
         cin >> seleccion;
+        while (seleccion != 'a' && seleccion != 'b'){
+            cout << "Escoja una opción válida: ";
+            cin >> seleccion;
+        }
         switch(seleccion){
         case 'a':
             cout << "1. Agregar" << endl;
@@ -104,18 +165,38 @@ int main(){
                 cin >> opcion;
             }
             if (opcion == 1){ //Agregar nuevo tipo de ventanillas con descripcion codigo y cantidad
-
+                    cout << "Escriba la descripción de la nueva ventanilla: ";
+                    string descripcionVentanilla;
+                    cin >> descripcionVentanilla;
+                    cout << "Escriba el código de la nueva ventanilla: ";
+                    string codigoVentanilla;
+                    cin >> codigoVentanilla;
+                    cout << "Escriba la cantidad de ventanillas deseada: ";
+                    int cantida;
+                    cin >> cantida;
+                    while (cantida < 1){
+                        cout << "Escriba la cantidad de ventanillas deseada (entero positivo): ";
+                        cin >> cantida;
+                    }
+                    TipoVentanilla *nueva = new TipoVentanilla(descripcionVentanilla, codigoVentanilla, cantida);
+                    totalVentanillas->append(nueva);
             }else { //Eliminar tipo de ventanillas y todas las ventanillas definidas
-
+                    imprimirTotalVentanillas(totalVentanillas);
+                    cout << "Ingrese el número de la Ventanilla que desea eliminar: ";
+                    cin >> opcion;
+                    while(opcion < 0 || opcion > totalVentanillas->getSize() - 1){
+                        cout << "Ingrese el número correcto de la Ventanilla que desea eliminar: ";
+                        cin >> opcion;
+                    }
+                    totalVentanillas->goToPos(opcion); //Eliminando el tipo de ventanilla
+                    TipoVentanilla *esta = totalVentanillas->remove();
+                    delete esta;
             }
             break;
         case 'b':
             cout << "1. Agregar un servicio" << endl;
             cout << "2. Eliminar un servicio" << endl;
-            cout << "3. Reordenar elementos en la lista.";
-            cout << "1. Agregar" << endl;
-            cout << "2. Eliminar" << endl;
-            cout << "3. Reordenar";
+            cout << "3. Reordenar elementos en la lista" << endl;
             cout << "Elija su opción: ";
             cin >> opcion;
             while (opcion < 1 || opcion > 3){
@@ -123,13 +204,40 @@ int main(){
                 cin >> opcion;
             }
             if (opcion == 1){
-                //Agregar un nuevo tipo de servicio
+                //Agregar un nuevo tipo de servicio con descripción y código
+                    cout << "Escriba la descripción del nuevo servicio: ";
+                    string descripcionServicio;
+                    cin >> descripcionServicio;
+                    cout << "Escriba el código del nuevo servicio: ";
+                    string codigoServicio;
+                    cin >> codigoServicio;
+                    Servicio *nuevo = new Servicio(descripcionServicio, codigoServicio);
+                    totalServicios->append(nuevo);
+
             }else if (opcion == 2){
                 //Eliminar un tipo de servicio
+                imprimirTotalServicios(totalServicios);
+                cout << "Ingrese el número del servicio que desea eliminar: ";
+                cin >> opcion;
+                while(opcion < 0 || opcion > totalServicios->getSize() - 1){
+                    cout << "Ingrese el número correcto del Servicio que desea eliminar: ";
+                    cin >> opcion;
+                }
+                totalServicios->goToPos(opcion); //Eliminando el tipo de ventanilla
+                Servicio *esta = totalServicios->remove();
+                delete esta;
             }else{
                 //Reordenar elementos en la lista de servicios
+                imprimirTotalServicios(totalServicios);
+                cout << "Indique que numero de Servicio desea reordenar: ";
+                cin >> opcion;
+                cout << "Indique la posición en la que desea que quede este servicio: ";
+                int posDestino;
+                cin >> posDestino;
+                totalServicios->goToStart();
+                Servicio *actual = totalServicios->getElement();
+                actual->reordenar(opcion, posDestino, totalServicios);
             }
-
             break;
         }
         break;
@@ -139,10 +247,26 @@ int main(){
         //Total de tiquetes atendidos por ventanilla
         //Total de tiquetes dispensados por tipo de transacción
         //Total de tiquetes preferenciales dispensados en todo el sistema
+        break;
     case 6:
         return 0;
     }
-
+    //Elección para decidir si el usuario se quiere salir o no
+    if (regresar == false){
+        cout << "¿Desea realizar algún otro trámite? (0 = no, 1 = sí): ";
+        int laDecision;
+        cin >> laDecision;
+        while (laDecision < 0 || laDecision > 1){
+            cout << "Debe ingresar alguna de las opciones indicadas." << endl;
+            cout << "¿Desea realizar algún otro trámite? (0 = no, 1 = sí): ";
+            cin >> laDecision;
+        }
+        if (laDecision == 0){
+            seguirDentro = false;
+            cout << "Gracias por visitar el Programa de administración de colas hecho por Juleisy y Javier." << endl;
+        }
+    }
+    }
         return 0;
 }
 
